7f5360fc5ba35f20c54db6c78ac96e214c7070f9
Michael Gailling <michael.gailling@gmail.com>
2020-11-09 00:39:56 -0500

    DB Update Updated Models Survey List View and display controller Survey Create View and display controller Survey Edit View and display controller Updated effected routes Updated Mongoose settings

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -4,6 +4,7 @@
  * Group: 2
  * Section: 3
  * File: ./controllers/survey.js
+ * 
 \*/
 
 let express = require('express');
@@ -12,6 +13,53 @@ let router = express.Router;
 //DB
 let mongoose = require('mongoose');
 
-module.exports.displayIndex = (req,res,next) => {
-    res.render('survey/index', { title: 'SurveyList'});
+let Survey = require('../models/survey');
+let Question = require('../models/question');
+
+module.exports.displaySurveyList = (req,res,next) => 
+{
+    Survey.find( (err, surveys) => {
+        if (err) {
+          return console.error(err);
+        }
+        else {
+            console.log(surveys[0].title);
+            console.log(surveys[0].questionlist);
+            res.render('survey/index', { 
+                title: 'Survey List', 
+                surveys: surveys
+            });
+        }
+      });
+}
+
+module.exports.displayCreateSurvey = (req,res,next) =>
+{
+    let newQuestion = new Question({
+        text:"New Question",
+        type:"text",
+        options:[]
+    });
+    let newSurvey = new Survey({
+        title:"New Survey"
+    });
+    return res.render('survey/details', {title: 'Create Survey', survey: newSurvey, questions:[newQuestion]});
+}
+
+module.exports.displayEditSurvey = (req,res,next) =>
+{
+    let id = req.params.id;
+  //Find the book based on record id
+  Survey.findById(id, (err, currentsurvey) => {
+      if(err)
+      {
+          console.error(err);
+          res.end(err);
+      }
+      else
+      {
+          let questions = currentsurvey.questionlist;
+          res.render('survey/details', {title : "Edit Survey", survey : currentsurvey, questions : questions});
+      }
+  });
 }
--- server/routes/survey.js
+++ server/routes/survey.js
@@ -12,6 +12,12 @@ var router = express.Router();
 let surveyController = require("../controllers/survey");
 
 /* GET home page. */
-router.get('/', surveyController.displayIndex);
+router.get('/', surveyController.displaySurveyList);
+
+/* GET create survey page */
+router.get('/create', surveyController.displayCreateSurvey);
+
+/* GET edit survey page */
+router.get('/edit/:id', surveyController.displayEditSurvey);
 
 module.exports = router;
