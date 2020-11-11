554c31c3c206bde6492021fdf865bd903823a274
Michael Gailling <michael.gailling@gmail.com>
2020-11-10 23:56:40 -0500

    Basic CRUD Controllers Create/Update - Working -Mutiple choice question still need to be imlmented Read - working Delete - Working

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -11,13 +11,22 @@ var router = express.Router();
 
 let surveyController = require("../controllers/survey");
 
-/* GET home page. */
+/* GET Survey List - READ */
 router.get('/', surveyController.displaySurveyList);
 
-/* GET create survey page */
+/* GET create survey page - CREATE */
 router.get('/create', surveyController.displayCreateSurvey);
 
-/* GET edit survey page */
+/*POST Route for creating and adding new survey to DB - CREATE */
+router.post('/create', surveyController.createSurvey);
+
+/* GET edit survey page - UPDATE */
 router.get('/edit/:id', surveyController.displayEditSurvey);
+
+/* POST Route for editing a Contact in db - UPDATE */
+router.post('/edit/:id', surveyController.editSurvey);
+
+/* GET Route for performing a Survey delete - DELETE */
+router.get('/delete/:id', surveyController.deleteSurvey);
 
 module.exports = router;
