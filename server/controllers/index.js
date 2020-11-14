02d8e528853f507155627fc7e8a3b89be0dbef4c
hanhStuff <48571430+hanhStuff@users.noreply.github.com>
2020-11-13 20:43:04 -0500

    added navbar and some styling

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -15,17 +15,5 @@ let Survey = require('../models/survey');
 
 module.exports.displayHomePage = (req,res,next) => {
     //Find and list all the surveys
-    Survey.find( (err, surveys) => {
-        if (err) {
-          return console.error(err);
-        }
-        else {
-            console.log(surveys[0].title);
-            console.log(surveys[0].questionlist);
-            res.render('home/index', { 
-                title: 'Home', 
-                surveys: surveys
-            });
-        }
-      });
+  res.render('home/index', {title: "Home"});
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,6 @@ let surveyController = require("../controllers/survey");
 
 
 /* GET home page. */
-router.get('/', surveyController.displaySurveyList);
+router.get('/', indexController.displayHomePage);
 
 module.exports = router;
